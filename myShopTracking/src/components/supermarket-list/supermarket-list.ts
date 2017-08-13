3a4c55450698b7a360048d6c4765a7bc57870413
DESKTOP-L53GNE2\jackp <jack.parra253@gmail.com>
2017-08-12 22:14:40 -0500

    Componente lista de supermercados.

--- myShopTracking/src/components/supermarket-list/supermarket-list.ts
+++ myShopTracking/src/components/supermarket-list/supermarket-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 import {Supermarket} from '../../interfaces/supermarket';
 import {SupermarketServiceProvider} from '../../providers/supermarket-service/supermarket-service';
@@ -17,10 +17,18 @@ export class SupermarketListComponent {
   constructor(private supermarketServiceProvider: SupermarketServiceProvider) { 
 
   }
+  @Output()
+  //public productChangeCount: EventEmitter<any> = new EventEmitter();
+  public selectSupermarket: EventEmitter<Supermarket> = new EventEmitter();
+
   ngOnInit(): void{
     this.supermarketServiceProvider.getSupermarket().then(result=>{
       this.supermarketList = result;
     });
+  }
+
+  marketSelect(select: Supermarket):void{
+    this.selectSupermarket.emit(select);
   }
   
 }
--- myShopTracking/src/providers/supermarket-service/supermarket-service.ts
+++ myShopTracking/src/providers/supermarket-service/supermarket-service.ts
@@ -30,9 +30,9 @@ export class SupermarketServiceProvider {
     });
   }
 
-  public getSupermarket(): Promise<Supermarket[]>{
+  public getSupermarket(): Promise<Supermarket[]> {
     return this.storage.get("supermarket")
-      .then(response=>response as Supermarket[]);
+      .then(response => response as Supermarket[]);
   }
-    
+
 }
