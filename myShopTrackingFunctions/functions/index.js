796e7e59968dfb2a0d61f19af9a8f1947998fdf9
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2017-09-20 06:53:02 -0500

    Refactorización completa de la estructura del proyecto

---
+++ myShopTrackingFunctions/functions/index.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.historyPrice = functions.database.ref('/products/{barcode}/supermarkets/{supermarket}').onWrite((snapshot) => {
+    const time = new Date();
+    const obj = {
+        date: time,
+        price: snapshot.data.val()
+    };
+
+    return admin.database().ref(`/history/${snapshot.params.barcode}/${snapshot.params.supermarket}/${time.getTime()}`).update(obj);
+});
