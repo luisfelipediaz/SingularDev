94f0a079bd83ef78088f81447b6a8c3ee2b12a5b
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2018-03-10 08:51:37 -0500

    Actuaizacion de paquetes

--- myShopTrackingFunctions/functions/index.js
+++ myShopTrackingFunctions/functions/index.js
@@ -3,29 +3,6 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-// exports.changeProduct = functions.firestore.document('/products/{barcode}').onWrite(snapshot => {
-//     if (snapshot.data.exists) {
-//         return admin.firestore().collection(`/products/${snapshot.params.barcode}/supermarkets`).get().then(supermarkets => {
-//             let product = snapshot.data.data();
-//             supermarkets.forEach(supermarket => {
-//                 let data = Object.assign({}, product);
-//                 data = Object.assign(data, supermarket.data());
-//                 data.id = null;
-//                 admin.firestore().doc(`/supermarkets/${supermarket.id}/products/${product.id}`).set(data).catch(err => {
-//                     console.log(err);
-//                 });
-//             });
-//         });
-//     }
-// });
-
 exports.historyPriceFS = functions.firestore.document('/products/{barcode}/supermarkets/{supermarket}').onWrite((snapshot) => {
     if (snapshot.data.exists) {
         const time = new Date();
